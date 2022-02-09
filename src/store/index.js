360ec8d37dfe7977119fc96406829235f8e98c61
Michiel Bouwmeester <michiel.bouwmeester@nl.abnamro.com>
2022-02-09 11:18:18 +0100

    090222: added vuex, router & axios

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    genres: ['Fantasy'],
+    shows: []
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
